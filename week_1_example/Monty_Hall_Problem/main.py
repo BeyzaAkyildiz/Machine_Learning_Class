9f568f4716f24dbd078e188ccde4ade917b3f5be
BeyzaAkyildiz <beyza_akyildizz@outlook.com>
2022-02-18 13:20:48 +0300

    Monty Hall Problemi

---
+++ week_1_example/Monty_Hall_Problem/main.py
@@ -0,0 +1,34 @@
+# Monty Hall Problem Çözümü
+import random
+# random olarak işaretli ve seçilmiş kapıyı seçilmesi
+isaretli = random.randint(0, 2)
+secilmis = random.randint(0, 2)
+temp = 0
+sonuc = 0
+# ilk seçimin seçilip kazanma hesabı
+for x in range(1000):
+    isaretli = random.randint(0, 2)
+    secilmis = random.randint(0, 2)
+    if (isaretli == secilmis):
+        temp += 1
+sonuc = (temp * 100) / 1000
+print(sonuc)
+tempp = 0
+hesap = 0
+# seçimin değiştirilme kazanma durumu hesabı
+for y in range(10000):
+    yeni = 0
+    digeri = 0
+    isaretlii = random.randint(0, 2)
+    secilmiss = random.randint(0, 2)
+    for z in range(3):
+
+        if ((z != secilmiss) and (z != isaretlii)):
+            yeni = z
+    for m in range(3):
+        if ((m != secilmiss) and (m != yeni)):
+            digeri = m
+    if (isaretlii == digeri):
+        tempp += 1
+hesap = (tempp * 100) / 10000
+print(hesap)
